d56d86b8cf29d8227e645631d127df12c0802660
Chad Furman <chad@chadfurman.com>
2015-09-12 19:44:35 -0400

    binding to serviceData instead of directive=this

--- app/main/directives/sound-button-dir.js
+++ app/main/directives/sound-button-dir.js
@@ -7,65 +7,69 @@ angular.module('main')
     scope: {
       soundAttr: '=sound'
     },
-    link: function postLink (scope, element, attrs) {
-      var directive = this;
-      var sound = new Audio(soundAttr.src);
-      var progress = new SoundProgress(sound);
-      var buttonRootElement = element[0];
-      var buttonTextContainerElement = buttonRootElement.firstChild;
-      var buttonTextElement = buttonTextContainerElement.firstChild;
-      var delay = delay || DEFAULT_PROGRESS_DELAY;
+    link: function postLink (scope, element) {
+      console.log('Sound directive');
+      console.log('Sound attr: ', scope.soundAttr);
+      console.log('Sound src: ', scope.soundAttr.src);
 
-      sound.loop = soundAttr.loop;
+      var serviceData = {};
+      serviceData.sound = new Audio(SOUND_DIR + '/' + scope.soundAttr.src);
+      serviceData.buttonRootElement = element[0];
+      serviceData.buttonTextContainerElement = serviceData.buttonRootElement.firstChild;
+      serviceData.buttonTextElement = serviceData.buttonTextContainerElement.firstChild;
+
+      serviceData.sound.loop = scope.soundAttr.loop;
 
       var hideStop = function () {
-        buttonTextElement.innerHTML = '';
+        serviceData.buttonTextElement.innerHTML = '';
         element.off('click', playStop);
       };
 
       var showStop = function () {
-        buttonTextElement.innerHTML = 'Stop';
+        serviceData.buttonTextElement.innerHTML = 'Stop';
         element.on('click', playStop);
       };
 
       var hidePlay = function () {
-        buttonTextElement.innerHtml = '';
+        serviceData.buttonTextElement.innerHtml = '';
         element.off('click', playStart);
       };
 
       var showPlay = function () {
-        buttonTextElement.innerHTML = 'Play';
+        serviceData.buttonTextElement.innerHTML = 'Play';
         element.on('click', playStart);
       };
 
       var createProgressBar = function () {
         var bar = document.createElement('div');
         bar.classList.add('progress');
-        buttonRootElement.appendChild(bar);
-        directive.progressBarElement = bar;
+        serviceData.buttonRootElement.appendChild(bar);
+        serviceData.progressBarElement = bar;
         updateProgressBar();
       };
 
       var startProgressBar = function () {
         console.log('starting progress bar');
         createProgressBar();
-        directive.progressBarInterval = $interval(function () {
+        serviceData.progressBarInterval = $interval(function () {
           updateProgressBar();
-        }, delay);
+        }, DEFAULT_PROGRESS_DELAY);
 
       };
 
       var stopProgressBar = function () {
-        directive.progressBarElement.remove();
-        delete directive.progressBarElement;
-        directive.progressBarInterval.cancel();
+        console.log('stopping progress bar');
+        serviceData.progressBarElement.remove();
+        delete serviceData.progressBarElement;
+        $interval.cancel(serviceData.progressBarInterval);
       };
 
       var updateProgressBar = function () {
-        directive.progressBarElement.width = progress.getProgress() + '%';
+        serviceData.progressBarElement.width = SoundProgress.getProgress(serviceData.sound) + '%';
       };
 
       var playStop = function () {
+        var sound = serviceData.sound;
         console.log('stopping sound', sound);
         sound.pause();
         sound.currentTime = 0;
@@ -75,6 +79,7 @@ angular.module('main')
       };
 
       var playStart = function () {
+        var sound = serviceData.sound;
         console.log('playing sound', sound);
         sound.play();
         hidePlay();

f24b839c85c433da9883a4843f3ed707bd73f627
Chad Furman <chad@chadfurman.com>
2015-09-12 19:41:42 -0400

    more logging in sound controller

--- app/main/controllers/sound-ctrl.js
+++ app/main/controllers/sound-ctrl.js
@@ -2,11 +2,15 @@
 angular.module('main')
 .controller('SoundCtrl', function () {
 
+  console.log('Sounds controller loaded');
+
   this.sounds = [
     {loop: true, src: 'default/bell.mp3'},
     {loop: true, src: 'default/bell.mp3'},
     {loop: false, src: 'default/bell.mp3'},
     {loop: false, src: 'default/bell.mp3'}
   ];
+
+  console.log('Sounds: ', this.sounds);
 
 });

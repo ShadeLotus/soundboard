0b4eb539a68e3113dd2aaf43604b0972f78eb5cf
Chad Furman <chad@chadfurman.com>
2015-09-13 17:15:41 -0400

    added protractor mocks config to conf.js

--- e2e/conf.js
+++ e2e/conf.js
@@ -1,5 +1,9 @@
 exports.config = {
   framework: 'jasmine2',
   seleniumAddress: 'http://localhost:4444/wd/hub',
-  specs: ['*.spec.js']
+  specs: ['*.spec.js'],
+  mocks: {
+    default: ['mock-login'],
+    dir: 'mocks'
+  }
 }

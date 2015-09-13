c05b589266abe18c6329310a15b6755ab776d8f2
Chad Furman <chad@chadfurman.com>
2015-09-12 23:25:01 -0400

    fixed validation and added logging to sound service

--- app/main/services/sound-progress-serv.js
+++ app/main/services/sound-progress-serv.js
@@ -4,14 +4,17 @@ angular.module('main')
   console.log('Hello from your Service: SoundProgress in module main');
 
   this.getProgress = function (sound) {
-    if (! sound.hasOwnProperty('currentTime')) {
+    if (typeof sound.currentTime === 'undefined') {
       console.log('sound object is not an audio element: ', sound);
       return;
     }
 
     console.log('getting sound progress');
     var maxTime = sound.duration,
-        currentTime = sound.currentTime;
-    return Math.ceil((currentTime / maxTime) * 100);
+        currentTime = sound.currentTime,
+        soundProgress = Math.ceil((currentTime / maxTime) * 100);
+    console.log('sound progress: ' + soundProgress);
+
+    return soundProgress;
   };
 });

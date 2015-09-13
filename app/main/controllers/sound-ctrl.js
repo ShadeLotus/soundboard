0c702e9aaf7cf174891026a9989674a78a80150a
Chad Furman <chad@chadfurman.com>
2015-09-12 23:20:17 -0400

    packages are exposed by sound controller instead of sounds

--- app/main/controllers/sound-ctrl.js
+++ app/main/controllers/sound-ctrl.js
@@ -4,13 +4,25 @@ angular.module('main')
 
   console.log('Sounds controller loaded');
 
-  this.sounds = [
-    {loop: true, src: 'default/bell.mp3'},
-    {loop: true, src: 'default/bell.mp3'},
-    {loop: false, src: 'default/bell.mp3'},
-    {loop: false, src: 'default/bell.mp3'}
-  ];
+  this.packages = {
+    'default': {
+      sounds: [
+        {loop: true, src: 'default/bell.mp3'},
+        {loop: true, src: 'default/bell.mp3'},
+        {loop: false, src: 'default/bell.mp3'},
+        {loop: false, src: 'default/bell.mp3'}
+      ]
+    },
+    'christmas': {
+      sounds: [
+        {loop: false, src: 'default/bell.mp3'},
+        {loop: false, src: 'default/bell.mp3'},
+        {loop: true, src: 'default/bell.mp3'},
+        {loop: true, src: 'default/bell.mp3'}
+      ]
+    }
+  };
 
-  console.log('Sounds: ', this.sounds);
+  console.log('Packages: ', this.packages);
 
 });

1b3f6cbc4014c9d4e26b484bfc2f0bdf54b65199
Chad Furman <chad@chadfurman.com>
2015-09-19 10:58:17 -0400

    API service returns $q

--- app/main/services/api-serv.js
+++ app/main/services/api-serv.js
@@ -1,12 +1,20 @@
 'use strict';
 angular.module('main')
-.service('Api', function (API_SERVER) {
+.service('Api', function (API_SERVER, $http, $q) {
 
   console.log('Hello from your Service: Api in module main');
   // TODO: do your service thing
   var ctrl = this;
 
   ctrl.getPackages = function () {
-    return $http.get(API_SERVER + '/packages');
+    return $q(function (resolve, reject) {
+      $http.get(API_SERVER + '/packages').then(
+        function (responseSuccess) {
+          resolve(responseSuccess.data);
+        },
+        function (responseFailure) {
+          reject(responseFailure);
+        });
+    });
   };
 });

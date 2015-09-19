5d0d9b0abfa5bc1b73d6087ed589ba487bc43654
Chad Furman <chad@chadfurman.com>
2015-09-19 10:55:51 -0400

    added mocks for http requests

--- app/app.js
+++ app/app.js
@@ -1,5 +1,29 @@
 'use strict';
 angular.module('Soundboard', [
-  // load your modules here
-  'main' // starting with the main module
-]);
+  'ngMockE2E',
+  'main'
+])
+.run(function ($httpBackend, API_SERVER) {
+  $httpBackend
+    .whenGET(API_SERVER + '/packages')
+    .respond({
+      default: {
+        sounds: [
+          {loop: true, src: 'default/bell.mp3'},
+          {loop: true, src: 'default/bell.mp3'},
+          {loop: false, src: 'default/bell.mp3'},
+          {loop: false, src: 'default/bell.mp3'}
+        ]
+      },
+      christmas: {
+        sounds: [
+          {loop: false, src: 'default/bell.mp3'},
+          {loop: false, src: 'default/bell.mp3'},
+          {loop: true, src: 'default/bell.mp3'},
+          {loop: true, src: 'default/bell.mp3'}
+        ]
+      }
+    });
+
+  $httpBackend.whenGET(/templates/).passThrough();
+});

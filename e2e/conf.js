cd39cc53687654f4e30bfd694cf7aae7627d2e12
Chad Furman <chad@chadfurman.com>
2015-09-19 10:59:55 -0400

    removing protractor / jasmine tests in favor of cucumber

--- e2e/conf.js
+++
@@ -1,9 +0,0 @@
-exports.config = {
-  framework: 'jasmine2',
-  seleniumAddress: 'http://localhost:4444/wd/hub',
-  specs: ['*.spec.js'],
-  mocks: {
-    default: ['mock-login'],
-    dir: 'mocks'
-  }
-}

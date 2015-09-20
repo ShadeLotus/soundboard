3fbbe80ac8a099a91c895da6889d6ffb3973d38b
Chad Furman <chad@chadfurman.com>
2015-09-19 22:01:40 -0400

    WIP: fork child process to be killed and re-forked later

--- features/support/world.js
+++ features/support/world.js
@@ -1,7 +1,17 @@
 // features/support/world.js
+require('dotenv').load();
 var zombie = require('zombie');
+var fork = require('child_process').fork;
+
 function World(callback) {
-  //TODO: bootstrap app with test database
+  //TODO: bootstrap api with test database
+  var child = fork('SOUNDBOARD_DATASOURCE=testDb node ' . process.env.API_APP_PATH);
+
+  // setup helper for wiping the database
+  // hrm so what's the best way to make a global method for cucumber...
+  // looks like it might be to attach it to 'this'
+  //basically, we want to make a custom datasource and... wipe it... between tests
+  //sails disk should be fine for this
 
   this.browser = new zombie(); // this.browser will be available in step definitions
 

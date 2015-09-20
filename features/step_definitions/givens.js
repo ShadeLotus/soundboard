1bc170d3bcc3dc03624f85b72ff7e3dc77fb0611
Chad Furman <chad@chadfurman.com>
2015-09-19 23:35:41 -0400

    first attempt at organizing step definitions

---
+++ features/step_definitions/givens.js
@@ -0,0 +1,51 @@
+module.exports = function() {
+  this.Given(/^the user is logged in$/, function (callback) {
+    // Write code here that turns the phrase above into concrete actions
+    callback.pending();
+  });
+
+  this.Given(/^the audio element for the first button is playing$/, function (callback) {
+    // Write code here that turns the phrase above into concrete actions
+    callback.pending();
+  });
+
+  this.Given(/^the current accessToken for the user is (\d+)\.(\d+)\.(\d+)\.(\d+)\.(\d+)\-thats\-amazing$/, function (arg1, arg2, arg3, arg4, arg5, callback) {
+    // Write code here that turns the phrase above into concrete actions
+    callback.pending();
+  });
+
+  this.Given(/^the following creators:$/, function (table, callback) {
+    // Write code here that turns the phrase above into concrete actions
+    callback.pending();
+  });
+
+  this.Given(/^the creator does not have an active session$/, function (callback) {
+    // Write code here that turns the phrase above into concrete actions
+    callback.pending();
+  });
+
+  this.Given(/^the creator is logged in$/, function (callback) {
+    // Write code here that turns the phrase above into concrete actions
+    callback.pending();
+  });
+
+  this.Given(/^the creator visits '\/'$/, function (callback) {
+    // Write code here that turns the phrase above into concrete actions
+    callback.pending();
+  });
+
+  this.Given(/^user chad@gmail\.com is logged in$/, function (callback) {
+    // Write code here that turns the phrase above into concrete actions
+    callback.pending();
+  });
+
+  this.Given(/^the following users:$/, function (table, callback) {
+    // Write code here that turns the phrase above into concrete actions
+    callback.pending();
+  });
+
+  this.Given(/^the user visits '\/packages'$/, function (callback) {
+    // Write code here that turns the phrase above into concrete actions
+    callback.pending();
+  });
+};
---
+++ features/step_definitions/whens.js
@@ -0,0 +1,222 @@
+module.exports = function() {
+  this.When(/^the user visits '\/'$/, function (callback) {
+    // Write code here that turns the phrase above into concrete actions
+    callback.pending();
+  });
+
+  this.When(/^the user clicks on the first button$/, function (callback) {
+    // Write code here that turns the phrase above into concrete actions
+    callback.pending();
+  });
+
+  this.When(/^the user clicks on the first button again to stop a sound$/, function (callback) {
+    // Write code here that turns the phrase above into concrete actions
+    callback.pending();
+  });
+
+  this.When(/^the user waits until the audio element for the first button has elapsed its duration$/, function (callback) {
+    // Write code here that turns the phrase above into concrete actions
+    callback.pending();
+  });
+
+  this.When(/^the user visits '\/' in the app$/, function (callback) {
+    // Write code here that turns the phrase above into concrete actions
+    callback.pending();
+  });
+
+  this.When(/^the creator visits '\/'$/, function (callback) {
+    // Write code here that turns the phrase above into concrete actions
+    callback.pending();
+  });
+
+  this.When(/^the creator clicks in the 'username' field$/, function (callback) {
+    // Write code here that turns the phrase above into concrete actions
+    callback.pending();
+  });
+
+  this.When(/^the creator types 'chad'$/, function (callback) {
+    // Write code here that turns the phrase above into concrete actions
+    callback.pending();
+  });
+
+  this.When(/^the creator clicks in the password field$/, function (callback) {
+    // Write code here that turns the phrase above into concrete actions
+    callback.pending();
+  });
+
+  this.When(/^the creator types '(\d+)'$/, function (arg1, callback) {
+    // Write code here that turns the phrase above into concrete actions
+    callback.pending();
+  });
+
+  this.When(/^the creator presses enter$/, function (callback) {
+    // Write code here that turns the phrase above into concrete actions
+    callback.pending();
+  });
+
+  this.When(/^the creator clicks the soundpack edit icon for 'cats'$/, function (callback) {
+    // Write code here that turns the phrase above into concrete actions
+    callback.pending();
+  });
+
+  this.When(/^the creator clicks the name edit icon for sound with id '(\d+)'$/, function (arg1, callback) {
+    // Write code here that turns the phrase above into concrete actions
+    callback.pending();
+  });
+
+  this.When(/^the creator types 'Candy' into the name edit field$/, function (callback) {
+    // Write code here that turns the phrase above into concrete actions
+    callback.pending();
+  });
+
+  this.When(/^the creator clicks save or presses enter$/, function (callback) {
+    // Write code here that turns the phrase above into concrete actions
+    callback.pending();
+  });
+
+  this.When(/^the creator clicks the loop status checkbox for sound with id '(\d+)'$/, function (arg1, callback) {
+    // Write code here that turns the phrase above into concrete actions
+    callback.pending();
+  });
+
+  this.When(/^the creator clicks the delete icon for sound with id '(\d+)'$/, function (arg1, callback) {
+    // Write code here that turns the phrase above into concrete actions
+    callback.pending();
+  });
+
+  this.When(/^the user confirms they wish to delete the sound$/, function (callback) {
+    // Write code here that turns the phrase above into concrete actions
+    callback.pending();
+  });
+
+  this.When(/^the creator clicks the 'add' button for soundpacks$/, function (callback) {
+    // Write code here that turns the phrase above into concrete actions
+    callback.pending();
+  });
+
+  this.When(/^the creator enters a unique name for the soundpack$/, function (callback) {
+    // Write code here that turns the phrase above into concrete actions
+    callback.pending();
+  });
+
+  this.When(/^the creator successfully uploads a video$/, function (callback) {
+    // Write code here that turns the phrase above into concrete actions
+    callback.pending();
+  });
+
+  this.When(/^the creator adds atleast one sound$/, function (callback) {
+    // Write code here that turns the phrase above into concrete actions
+    callback.pending();
+  });
+
+  this.When(/^the creator clicks the 'delete' icon for soundpack with id (\d+)$/, function (arg1, callback) {
+    // Write code here that turns the phrase above into concrete actions
+    callback.pending();
+  });
+
+  this.When(/^the creator confirms the delete$/, function (callback) {
+    // Write code here that turns the phrase above into concrete actions
+    callback.pending();
+  });
+
+  this.When(/^the creator clicks the 'edit' icon for soundpack with id '(\d+)'$/, function (arg1, callback) {
+    // Write code here that turns the phrase above into concrete actions
+    callback.pending();
+  });
+
+  this.When(/^the creator clicks in the edit field for soundpack with id '(\d+)'$/, function (arg1, callback) {
+    // Write code here that turns the phrase above into concrete actions
+    callback.pending();
+  });
+
+  this.When(/^the creator enters the text 'flowers' into the name field$/, function (callback) {
+    // Write code here that turns the phrase above into concrete actions
+    callback.pending();
+  });
+
+  this.When(/^the creator clicks save$/, function (callback) {
+    // Write code here that turns the phrase above into concrete actions
+    callback.pending();
+  });
+
+  this.When(/^the creator clicks the thumbnail edit icon$/, function (callback) {
+    // Write code here that turns the phrase above into concrete actions
+    callback.pending();
+  });
+
+  this.When(/^the user waits for half of the sound's duration$/, function (callback) {
+    // Write code here that turns the phrase above into concrete actions
+    callback.pending();
+  });
+
+  this.When(/^the user clicks on the type\-ahead list$/, function (callback) {
+    // Write code here that turns the phrase above into concrete actions
+    callback.pending();
+  });
+
+  this.When(/^the user clicks in the type\-ahead text field$/, function (callback) {
+    // Write code here that turns the phrase above into concrete actions
+    callback.pending();
+  });
+
+  this.When(/^the user enters 'christmas' in the text field$/, function (callback) {
+    // Write code here that turns the phrase above into concrete actions
+    callback.pending();
+  });
+
+  this.When(/^the user clicks on the 'christmas' soundpack in the type\-ahead list$/, function (callback) {
+    // Write code here that turns the phrase above into concrete actions
+    callback.pending();
+  });
+
+  this.When(/^the user clicks the video thumbnail on a sound package$/, function (callback) {
+    // Write code here that turns the phrase above into concrete actions
+    callback.pending();
+  });
+
+  this.When(/^the user long\-presses anywhere on the package$/, function (callback) {
+    // Write code here that turns the phrase above into concrete actions
+    callback.pending();
+  });
+
+  this.When(/^the user clicks in the search box$/, function (callback) {
+    // Write code here that turns the phrase above into concrete actions
+    callback.pending();
+  });
+
+  this.When(/^the user types 'cats'$/, function (callback) {
+    // Write code here that turns the phrase above into concrete actions
+    callback.pending();
+  });
+
+  this.When(/^the user sees only the 'cats' package$/, function (callback) {
+    // Write code here that turns the phrase above into concrete actions
+    callback.pending();
+  });
+
+  this.When(/^the user deletes 'cats' form the search box$/, function (callback) {
+    // Write code here that turns the phrase above into concrete actions
+    callback.pending();
+  });
+
+  this.When(/^the user clicks on the purchase button for the 'christmas' package$/, function (callback) {
+    // Write code here that turns the phrase above into concrete actions
+    callback.pending();
+  });
+
+  this.When(/^the user has confirmed purchase of 'christmas' package$/, function (callback) {
+    // Write code here that turns the phrase above into concrete actions
+    callback.pending();
+  });
+
+  this.When(/^the request for payment has been approved$/, function (callback) {
+    // Write code here that turns the phrase above into concrete actions
+    callback.pending();
+  });
+
+  this.When(/^the user 'chad@gmail\.com' has default_package_id (\d+)$/, function (arg1, callback) {
+    // Write code here that turns the phrase above into concrete actions
+    callback.pending();
+  });
+
+}

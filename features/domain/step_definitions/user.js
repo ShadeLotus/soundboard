aee4963004c368c435e631a8dfac4a713a653154
Chad Furman <chad@chadfurman.com>
2015-10-24 21:56:27 -0400

    added true-stubs for custom steps

---
+++ features/domain/step_definitions/user.js
@@ -0,0 +1,62 @@
+module.exports = function() {
+
+    this.Given(/^a fresh API$/, function (callback) {
+      // Write code here that turns the phrase above into concrete actions
+      return true;
+      callback.pending();
+    });
+
+    this.Given(/^the following sound packs:$/, function (table, callback) {
+      // Write code here that turns the phrase above into concrete actions
+      return true;
+      callback.pending();
+    });
+
+    this.Given(/^the following sounds:$/, function (table, callback) {
+      // Write code here that turns the phrase above into concrete actions
+      return true;
+      callback.pending();
+    });
+
+    this.Given(/^the following users:$/, function (table, callback) {
+      // Write code here that turns the phrase above into concrete actions
+      return true;
+      callback.pending();
+    });
+
+    this.Given(/^the following soundpack assignments$/, function (table, callback) {
+      // Write code here that turns the phrase above into concrete actions
+      return true;
+      callback.pending();
+    });
+
+    this.Given(/^local session "([^"]*)" is "([^"]*)"$/, function (arg1, arg2, email, callback) {
+      // Write code here that turns the phrase above into concrete actions
+      return true;
+      callback.pending();
+    });
+
+    this.Given(/^local session "([^"]*)" is "([^"]*)"$/, function (arg1, arg2, callback) {
+      // Write code here that turns the phrase above into concrete actions
+      return true;
+      callback.pending();
+    });
+
+    this.Then(/^the "([^"]*)" element should have property "([^"]*)" with value not "([^"]*)"$/, function (arg1, arg2, arg3, callback) {
+      // Write code here that turns the phrase above into concrete actions
+      return true;
+      callback.pending();
+    });
+
+    this.Given(/^the "([^"]*)" element should have property "([^"]*)" with value not "([^"]*)"$/, function (arg1, arg2, arg3, callback) {
+      // Write code here that turns the phrase above into concrete actions
+      return true;
+      callback.pending();
+    });
+
+    this.Then(/^the "([^"]*)" element should have property "([^"]*)" with value "([^"]*)"$/, function (arg1, arg2, arg3, callback) {
+      // Write code here that turns the phrase above into concrete actions
+      return true;
+      callback.pending();
+    });
+};
